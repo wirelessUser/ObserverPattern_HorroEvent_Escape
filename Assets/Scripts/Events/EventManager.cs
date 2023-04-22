2e56c93a4358a7ee1907615763a5d1ed05a9cbe3
mridul-outscal <128961934+mridul-outscal@users.noreply.github.com>
2023-04-22 21:38:24 +0530

    made every action an event Action in EventManager

--- Assets/Scripts/Events/EventManager.cs
+++ Assets/Scripts/Events/EventManager.cs
@@ -5,20 +5,17 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
-    // TODO - > Convert each Action into event Action , I have made EventManager as Singleton
-    [SerializeField] protected int keysRequiredToTrigger;
-    protected Collider triggerCollider;
 
     //Core Game Events
-    public static Action OnLightsOffByGhost;
-    public static Action OnSkullDrop;
-    public static Action OnRatRush;
-    public static Action OnPlayerEscaped;
+    public static event Action OnLightsOffByGhost;
+    public static event Action OnSkullDrop;
+    public static event Action OnRatRush;
+    public static event Action OnPlayerEscaped;
 
     //Interactable Events
-    public static Action<int> OnKeyPickedUp;
+    public static event Action<int> OnKeyPickedUp;
     public static event Action OnPotionDrink;
-    public static Action<bool> OnLightsSwitchToggled;
+    public static event Action<bool> OnLightsSwitchToggled;
 
 
     public static EventManager instance;
@@ -34,7 +31,6 @@ public class EventManager : MonoBehaviour
             Destroy(this.gameObject);
             Debug.LogError("Event Manager instance already exists");
         }
-        triggerCollider = GetComponent<Collider>();
     }
 
 
@@ -43,5 +39,34 @@ public class EventManager : MonoBehaviour
         OnPotionDrink?.Invoke();
     }
 
+    public void InvokeOnSkullDrop()
+    {
+        OnSkullDrop?.Invoke();
+    }
+
+    public void InvokeOnRatRush()
+    {
+        OnRatRush?.Invoke();
+    }
+
+    public void InvokeOnPlayerEscaped()
+    {
+        OnPlayerEscaped?.Invoke();
+    }
+
+    public void InvokeOnLightsOffByGhost()
+    {
+        OnLightsOffByGhost?.Invoke();
+    }
+
+    public void InvokeOnKeyPickedUp(int keysEquipped)
+    {
+        OnKeyPickedUp?.Invoke(keysEquipped);
+    }
+
+    public void InvokeOnLightsSwitchToggled(bool isSwitchOn)
+    {
+        OnLightsSwitchToggled?.Invoke(isSwitchOn);
+    }
 
 }
