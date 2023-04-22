67c1771729b06676a6013cdcdeae25668369d55f
MalharDevasthali <malhardevasthali007@gmail.com>
2023-04-22 19:49:43 +0530

    Reanamed

--- Assets/Scripts/Events/PlayerEventTrigger.cs
+++ Assets/Scripts/Events/PlayerEventTrigger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInteractedEventTrigger : EventManager
+public class PlayerEventTrigger : EventManager
 {
     private bool Isinteracted;
 
