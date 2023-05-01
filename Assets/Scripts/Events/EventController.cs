487c0342b88f2f293966236d3a65e6c315129ca5
MalharDevasthali <malhardevasthali007@gmail.com>
2023-05-01 22:59:13 +0530

    EventController

--- Assets/Scripts/Events/EventController.cs
+++ Assets/Scripts/Events/EventController.cs
@@ -3,37 +3,16 @@ using System;
 public class GameEventController<T>
 {
     protected event Action<T> baseEvent;
-    public void InvokeEvent(T type)
-    {
-        baseEvent?.Invoke(type);
-    }
-
-    public void AddListener(Action<T> listener)
-    {
-        baseEvent += listener;
-    }
-
-    public void RemoveListener(Action<T> listener)
-    {
-        baseEvent -= listener;
-    }
+    public void InvokeEvent(T type) => baseEvent?.Invoke(type);
+    public void AddListener(Action<T> listener) => baseEvent += listener;
+    public void RemoveListener(Action<T> listener) => baseEvent -= listener;
 }
 
 public class GameEventController
 {
     protected event Action baseEvent;
-    public void InvokeEvent()
-    {
-        baseEvent?.Invoke();
-    }
+    public void InvokeEvent() => baseEvent?.Invoke();
+    public void AddListener(Action listener) => baseEvent += listener;
+    public void RemoveListener(Action listener) => baseEvent -= listener;
 
-    public void AddListener(Action listener)
-    {
-        baseEvent += listener;
-    }
-
-    public void RemoveListener(Action listener)
-    {
-        baseEvent -= listener;
-    }
 }
