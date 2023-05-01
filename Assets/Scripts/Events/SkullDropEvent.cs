b7dbc93d09796c8249ce21f7ded76e6647ecd379
MalharDevasthali <malhardevasthali007@gmail.com>
2023-05-01 23:12:02 +0530

    Naming Improvements

--- Assets/Scripts/Events/SkullDropEvent.cs
+++ Assets/Scripts/Events/SkullDropEvent.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SkullDropEvent : MonoBehaviour
 {
     [SerializeField] private int keysRequiredToTrigger;
-    [SerializeField] private Transform Skulls;
+    [SerializeField] private Transform skulls;
     [SerializeField] private SoundType soundToPlay;
 
     private void OnTriggerEnter(Collider other)
@@ -19,6 +19,6 @@ public class SkullDropEvent : MonoBehaviour
 
     private void OnSkullDrop()
     {
-        Skulls.gameObject.SetActive(true);
+        skulls.gameObject.SetActive(true);
     }
 }
