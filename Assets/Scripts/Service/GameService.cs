c780b51a70f7dedd25f6def75eed6f4e1966980c
MalharDevasthali <malhardevasthali007@gmail.com>
2023-05-02 09:35:13 +0530

    Todo - Ask Mayank

--- Assets/Scripts/Service/GameService.cs
+++ Assets/Scripts/Service/GameService.cs
@@ -16,17 +16,15 @@ public class GameService : GenericMonoSingleton<GameService>
     [Header("Scriptable Objects")]
     [SerializeField] private PlayerScriptableObject playerScriptableObject;
 
+    private void Start() => playerController = new PlayerController(playerView, playerScriptableObject);
 
-    private void Start()
-    {
-        playerController = new PlayerController(playerView, playerScriptableObject);
-    }
+    //Todo - Ask Mayank -> Converting all these into properties as {get;private set;} is good or 
+    // we can keep it same for now?
 
     public PlayerController GetPlayerController()
     {
         return playerController;
     }
-
     public GameUIView GetGameUI()
     {
         return gameUIView;
@@ -35,15 +33,13 @@ public class GameService : GenericMonoSingleton<GameService>
     {
         return instructionView;
     }
-
     public SoundView GetSoundView()
     {
         return soundView;
     }
     public void GameOver()
     {
-        Debug.Log("Player Died");
-        playerController.KillPlayer(); // playerstate is state - outside classes should not be changing state from outside!
+        playerController.KillPlayer();
         soundView.PlaySoundEffects(SoundType.JumpScare1);
     }
 }
