065a9dd4e9adcdf790261973977aa68e1426e2c0
MalharDevasthali <malhardevasthali007@gmail.com>
2023-05-02 12:31:00 +0530

    MainMenuUI Improved

--- Assets/Scripts/UI/MainMenuUIView.cs
+++ Assets/Scripts/UI/MainMenuUIView.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 public class MainMenuUIView : MonoBehaviour
 {
-    [SerializeField] Button playButton;
+    [SerializeField] private Button playButton;
 
     private void OnEnable() => playButton.onClick.AddListener(OnPlayButtonClicked);
-
     private void OnPlayButtonClicked() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
 }
