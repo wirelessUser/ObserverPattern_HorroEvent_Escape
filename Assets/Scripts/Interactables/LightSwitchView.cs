86eeb1ed1fd97dbfb88db4434b459a6b2ec6a65e
MalharDevasthali <malhardevasthali007@gmail.com>
2023-05-02 17:52:27 +0530

    LightSwitchTodo Setup

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -6,21 +6,11 @@ public partial class LightSwitchView : MonoBehaviour, IInteractable
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
 
-    public delegate void onLightSwitchToggle();
-    public onLightSwitchToggle switchToggle;
+    private void Start() => currentState = SwitchState.Off;
 
-    private void OnEnable()
-    {
-        switchToggle = OnLightsToggled;
-    }
-    private void Start()
-    {
-        currentState = SwitchState.Off;
-    }
     public void Interact()
     {
-        GameService.Instance.GetInstructionView().HideInstruction();
-        switchToggle.Invoke();
+        //Todo - Implement Interaction
     }
     private void toggleLights()
     {
@@ -43,23 +33,5 @@ public partial class LightSwitchView : MonoBehaviour, IInteractable
         {
             lightSource.enabled = lights;
         }
-    }
-
-    private void SetLights(bool lights)
-    {
-        if (lights)
-            currentState = SwitchState.On;
-        else
-            currentState = SwitchState.Off;
-
-        foreach (Light lightSource in lightsources)
-        {
-            lightSource.enabled = lights;
-        }
-    }
-    private void OnLightsToggled()
-    {
-        toggleLights();
-        GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.SwitchSound);
     }
 }
