2c0f7897a6047ae953e7443e596ddfd13d66ed56
MalharDevasthali <malhardevasthali007@gmail.com>
2023-05-16 13:18:26 +0530

    Improved Keys Hardcoding

--- Assets/Scripts/Interactables/KeyView.cs
+++ Assets/Scripts/Interactables/KeyView.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class KeyView : MonoBehaviour, IInteractable
 {
+    [SerializeField] private SoundType soundType;
     public void Interact()
     {
         int currentKeys = GameService.Instance.GetPlayerController().KeysEquipped;
         GameService.Instance.GetInstructionView().HideInstruction();
-        GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.KeyPickUp);
+        GameService.Instance.GetSoundView().PlaySoundEffects(soundType);
         EventService.Instance.KeyPickedUpEvent.InvokeEvent(++currentKeys);
         gameObject.SetActive(false);
     }
