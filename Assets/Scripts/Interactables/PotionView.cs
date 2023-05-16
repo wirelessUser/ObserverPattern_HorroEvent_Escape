ea8df37901724b7e19da0099cce4c9d0cbb84727
MalharDevasthali <malhardevasthali007@gmail.com>
2023-05-16 13:21:31 +0530

    Improved potion hardcoding

--- Assets/Scripts/Interactables/PotionView.cs
+++ Assets/Scripts/Interactables/PotionView.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class PotionView : MonoBehaviour, IInteractable
 {
+    [SerializeField] SoundType soundType;
     private int potionEffect = 20;
+
     public void Interact()
     {
         GameService.Instance.GetInstructionView().HideInstruction();
-        GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.DrinkPotion);
+        GameService.Instance.GetSoundView().PlaySoundEffects(soundType);
         EventService.Instance.PotionDrinkEvent.InvokeEvent(potionEffect);
         gameObject.SetActive(false);
     }
