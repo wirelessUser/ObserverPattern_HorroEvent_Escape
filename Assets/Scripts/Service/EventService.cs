296b45acef6d826164afc38bf48b23d7669b5c43
MalharDevasthali <malhardevasthali007@gmail.com>
2023-05-17 16:12:55 +0530

    Naming Improvements

--- Assets/Scripts/Service/EventService.cs
+++ Assets/Scripts/Service/EventService.cs
@@ -13,20 +13,20 @@ public class EventService
         }
     }
 
-    public GameEventController OnLightSwitchToggled { get; private set; }
-    public GameEventController<int> OnKeyPickedUp { get; private set; }
-    public GameEventController LightsOffByGhostEvent { get; private set; }
+    public EventController OnLightSwitchToggled { get; private set; }
+    public EventController<int> OnKeyPickedUp { get; private set; }
+    public EventController LightsOffByGhostEvent { get; private set; }
 
-    public GameEventController PlayerEscapedEvent { get; private set; }
-    public GameEventController PlayerDeathEvent { get; private set; }
+    public EventController PlayerEscapedEvent { get; private set; }
+    public EventController PlayerDeathEvent { get; private set; }
 
     public EventService()
     {
-        OnLightSwitchToggled = new GameEventController();
-        OnKeyPickedUp = new GameEventController<int>();
-        LightsOffByGhostEvent = new GameEventController();
+        OnLightSwitchToggled = new EventController();
+        OnKeyPickedUp = new EventController<int>();
+        LightsOffByGhostEvent = new EventController();
 
-        PlayerEscapedEvent = new GameEventController();
-        PlayerDeathEvent = new GameEventController();
+        PlayerEscapedEvent = new EventController();
+        PlayerDeathEvent = new EventController();
     }
 }
